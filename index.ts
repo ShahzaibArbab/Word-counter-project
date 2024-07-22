c7595473d4c08580c13027c122d12829e86d4f26
ShahzaibArbab <Kingkhanjumma@gmail.com>
2024-07-22 20:43:32 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer:{
+    sentence:string
+}=await inquirer.prompt(
+    [
+        {
+            name:"sentence",
+            type:"input",
+            message:"Enter the sentence to count the words:"
+        }
+    ]);
+
+    const words = answer.sentence.trim().split(" ")
+    console.log(words);
+    console.log(`Your sentence word count is :${words.length}`);
